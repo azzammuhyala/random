de2137dec4ed702c8a523c0cec487b8037cbe334
azzammuhyala <143919179+azzammuhyala@users.noreply.github.com>
2026-08-24 20:53:35 +0700

    Update solusi.py

--- solusi.py
+++ solusi.py
@@ -1,8 +1,9 @@
 # @azzammuhyala - Bro
 
-input = "LVIII"
+# ILMU baru: len(x) itu mendaaptkan panjang string/list, contoh: mau cari berapa banyak/panjang karakter 'LVIII'? len(...) menghasilkan 5
 
-map = {
+angka_romawi = "LVIII"
+mapping_romawi = {
     #    vvvv --> bobot
     'I': 1,
     'V': 5,
@@ -14,18 +15,18 @@ map = {
 #   ^^^ --> karakter romawi
 }
 
-sum = 0
+total = 0
 
-for i in range(len(input)):
-    bobot = map[input[i]]
-    selanjutnya = i + 1
+for i in range(len(angka_romawi)):
+    bobot = mapping_romawi[angka_romawi[i]]
+    indeks_selanjutnya = i + 1
     #  +-- ini buat cek apakah indeks selanjutnya lebih kecil dari panjkang input biar gak terjadi error (IndexError)
-    #  |                            +--> bandingin nilai bobot karakter di indeks i sekarang dengan karakter indeks selanjutnya
-    #  |                            |    (aturannya)
-    #  vvvvvvvvvvvvvvvvvvvvvvvv     vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv 
-    if selanjutnya < len(input) and bobot < map[input[selanjutnya]]:
-        sum -= bobot # kalo semua kondisi di atas True (benar) maka kurangi bobot karakter sekarang
+    #  |                                          +--> bandingin nilai bobot karakter di indeks i sekarang dengan bobot karakter indeks selanjutnya
+    #  |                                          |    (aturannya)
+    #  vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv     vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+    if indeks_selanjutnya < len(angka_romawi) and bobot < mapping_romawi[angka_romawi[indeks_selanjutnya]]:
+        total -= bobot # kalo semua kondisi di atas True (benar) maka kurangi bobot karakter sekarang
     else:
-        sum += bobot # kalo gak ya jumlahin aja
+        total += bobot # kalo gak ya jumlahin aja
 
-print(sum)
+print(total)
